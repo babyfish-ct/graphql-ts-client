c735177c0368b466365d22449298157f861730b9
= <=>
2021-09-12 23:45:46 +0800

    relay-demo

--- example/client/relay-demo/src/common/RefreshFragment.ts
+++ example/client/relay-demo/src/common/RefreshFragment.ts
@@ -2,22 +2,22 @@ import { useEffect } from "react";
 import { Options } from "react-relay/relay-hooks/useRefetchableFragmentNode";
 import { DataID, Variables } from "relay-runtime";
 
-export function refreshFragment(id: DataID) {
-    window.dispatchEvent(new CustomEvent(REFRESH_FRAGMENT, { detail: {id}}));
+export function refreshFragment(id: DataID, reason?: string) {
+    window.dispatchEvent(new CustomEvent(EVENT_KEY_REFRESH_FRAGMENT, { detail: {id, reason}}));
 }
 
-export function useFragmentRefresher(id: DataID, refetch: (variables: Variables, options?: Options) => void) {
+export function useFragmentRefresher(id: DataID, reason: string | undefined, refetch: (variables: Variables, options?: Options) => void) {
     useEffect(() => {
         const handler = (e: CustomEvent) => {
-            if (e.detail.id === id) {
+            if (e.detail.id === id && e.detail.reason === reason) {
                 refetch({}, {fetchPolicy: "network-only"});
             }
         };
-        (window as any).addEventListener(REFRESH_FRAGMENT, handler);
+        (window as any).addEventListener(EVENT_KEY_REFRESH_FRAGMENT, handler);
         return () => {
-            (window as any).removeEventListener(REFRESH_FRAGMENT, handler);
+            (window as any).removeEventListener(EVENT_KEY_REFRESH_FRAGMENT, handler);
         }
     }, [id, refetch]);
 }
 
-const REFRESH_FRAGMENT = "refreshFragment";
+const EVENT_KEY_REFRESH_FRAGMENT = "refreshFragment";
---
+++ example/client/relay-demo/src/department/AvgSalaryText.tsx
@@ -0,0 +1,26 @@
+import { FC, memo } from "react";
+import { useFragmentRefresher } from "../common/RefreshFragment";
+import { createTypedFragment, FragmentKeyOf, useTypedRefetchableFragment } from "../__generated";
+import { department$ } from "../__generated/fetchers";
+import { REASON_COMPUTED_AVG_SALARY } from "../common/RefreshFragmentReason";
+
+export const AVG_SALARY_FRAGMENT = createTypedFragment(
+    "AvgSalaryFragment",
+    department$
+    .directive("refetchable", { queryName: "AvgSalaryRefetchQuery" })
+    .id
+    .avgSalary
+);
+
+export const AvgSalaryText: FC<{
+    department: FragmentKeyOf<typeof AVG_SALARY_FRAGMENT>
+}> = memo(({department}) => {
+
+    const [data, refetch] = useTypedRefetchableFragment(AVG_SALARY_FRAGMENT, department);
+
+    useFragmentRefresher(data.id, REASON_COMPUTED_AVG_SALARY, refetch);
+
+    return (
+        <span>{data.avgSalary}</span>
+    );
+});
